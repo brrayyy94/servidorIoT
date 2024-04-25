96c80df2bce3a45d29962986bca7d695626a0aed
brrayyy94 <brayan.maca@uao.edu.co>
2024-04-25 12:58:38 -0500

    logica para loguear usuario y registrarlo

--- src/routes/user/network.js
+++ src/routes/user/network.js
@@ -19,4 +19,67 @@ router.get("/", (req, res) => {
   success(res, "Todo correcto en /login", 200); // Responde con un mensaje de éxito
 });
 
+router.post("/login", (req, res) => {
+  const { email, password } = req.body; // Obtiene los datos del cuerpo de la petición
+
+  connection.getConnection((error, tempConn) => {
+    if (error) {
+      res.status(500).send("Error al conectar a la base de datos.");
+    } else {
+      console.log("Conexión correcta.");
+
+      // Consulta para obtener el usuario con el email y password proporcionados
+      const query = `
+          SELECT * FROM usuarios
+          WHERE user = ? AND password = ?`;
+
+      tempConn.query(query, [email, password], (error, result) => {
+        if (error) {
+          res.status(500).send("Error en la ejecución del query.");
+        } else {
+          tempConn.release(); // Liberar la conexión
+
+          if (result.length > 0) {
+            res.json(result); // Devolver los registros como respuesta JSON
+          } else {
+            res
+              .status(404)
+              .json({
+                mensaje: "No se encontraron registros con ese email y password.",
+              });
+          }
+        }
+      });
+    }
+  });
+});
+
+router.post("/register", (req, res) => {
+  const { email, password } = req.body;
+
+  connection.getConnection((error, tempConn) => {
+    if (error) {
+      res.status(500).send("Error al conectar a la base de datos.");
+    } else {
+      console.log("Conexión correcta.");
+
+      const query = `
+          INSERT INTO usuarios (user, password)
+          VALUES (?, ?)`;
+
+      tempConn.query(query, [email, password], (error, result) => {
+        if (error) {
+          res.status(500).send("Error en la ejecución del query.");
+        } else {
+          tempConn.release();
+
+          res.json({
+            mensaje: "Usuario registrado correctamente.",
+          });
+        }
+      });
+    }
+  });
+});
+
 module.exports = router; // Exporta el router con las rutas configuradas
